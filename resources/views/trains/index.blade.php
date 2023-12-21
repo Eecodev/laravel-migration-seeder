fda28bd51f974a14a6a36dbd732c077fc922d826
Eecodev <mnasser.bbk@gmail.com>
2023-12-21 18:28:27 +0100

    data printed on page

---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Trains')
+
+@section('content')
+<main>
+    <h1>Trains</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Company</th>
+            <th scope="col">Departing From</th>
+            <th scope="col">Arriving At</th>
+            <th scope="col">Departure time</th>
+            <th scope="col">Arrival Time</th>
+            <th scope="col">Train Number</th>
+            <th scope="col">Handle</th>
+            <th scope="col">Railroad car number</th>
+            <th scope="col">On time</th>
+            <th scope="col">Cancelled</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{$train->train_company}}</td>
+                    <td>{{$train->departure_station}}</td>
+                    <td>{{$train->arrival_station}}</td>
+                    <td>{{$train->departure_time}}</td>
+                    <td>{{$train->arrival_time}}</td>
+                    <td>{{$train->train_code}}</td>
+                    <td>{{$train->railroad_car_number}}</td>
+                    <td>{{$train->on_time}}</td>
+                    <td>{{$train->cancelled}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
+</main>
+@endsection
